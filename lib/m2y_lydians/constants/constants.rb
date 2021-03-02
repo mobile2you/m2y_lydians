a9d41606fa6b3a06a5cd2ea853fda715ce6af1c3
Danilo Sobral <danilo.sobral@mobile2you.com.br>
2021-03-02 11:40:52 -0300

    Added access password endpoints

--- lib/m2y_lydians/constants/constants.rb
+++ lib/m2y_lydians/constants/constants.rb
@@ -1,8 +1,12 @@
 module M2yLydians
   # Registration methods
+  BASE_AUTHENTICATOR_PATH = 'Aut.svc'
   UPDATE_CREDENTIALS_PATH = '/AtualizarCredencial_37'
-  CHANGE_PASSWORD_PATH = '/AlterarSenhaAcesso_37'
+  CREATE_ACCESS_PASSWORD_PATH = '/AlterarSenhaAcesso_37'
+  CHECK_ACCESS_PASSWORD_PATH = '/ValidarSenhaAcesso_37'
+  UPDATE_ACCESS_PASSWORD_PATH = '/AtualizarSenhaAcesso_37'
 
   # Account methods
+  BASE_ACCOUNT_PATH = 'Coc.svc'
   CREATE_ACCOUNT_PATH = '/IncluirCCDigital'
 end
--- lib/m2y_lydians/modules/registration.rb
+++ lib/m2y_lydians/modules/registration.rb
@@ -1,11 +1,23 @@
 module M2yLydians
   class Registration < Base
+    # /AtualizarCredencial_37
     def self.update_credentials(body)
-      post(base_url + UPDATE_CREDENTIALS_PATH, body).parsed_response
+      post(base_url + BASE_AUTHENTICATOR_PATH + UPDATE_CREDENTIALS_PATH, parsed_body(body))
     end
 
-    def self.change_password(body)
-      post(base_url + CHANGE_PASSWORD_PATH, body).parsed_response
+    # /AlterarSenhaAcesso_37
+    def self.create_access_password(body)
+      post(base_url + BASE_AUTHENTICATOR_PATH + CREATE_ACCESS_PASSWORD_PATH, parsed_body(body))
+    end
+
+    # /ValidarSenhaAcesso_37
+    def self.check_access_password(body)
+      post(base_url + BASE_AUTHENTICATOR_PATH + CHECK_ACCESS_PASSWORD_PATH, parsed_body(body))
+    end
+
+    # /AtualizarSenhaAcesso_37
+    def self.update_access_password(body)
+      post(base_url + BASE_AUTHENTICATOR_PATH + UPDATE_ACCESS_PASSWORD_PATH, parsed_body(body))
     end
   end
 end
