e306b088333f00f05aa3813f89aa77bbbd0eb28d
Danilo Sobral <danilo.sobral@mobile2you.com.br>
2021-03-02 11:38:51 -0300

    Added parsing for JSON response

--- lib/m2y_lydians/modules/base.rb
+++ lib/m2y_lydians/modules/base.rb
@@ -1,28 +1,39 @@
 module M2yLydians
   class Base
     def self.base_url
-      M2yLydians.Configuration.server_url
+      M2yLydians.configuration.server_url
     end
 
     def self.base_headers
       headers = {}
       headers['Content-Type'] = 'application/json'
-      headers['LYD-Certificado'] = M2yLydians.Configuration.certification
+      headers['LYD-Certificado'] = M2yLydians.configuration.certification
       headers
     end
 
     def self.post(url, body, headers = nil)
       headers = base_headers if headers.nil?
-      puts url.to_s
-      response = HTTParty.post(url, headers: headers, body: body)
-      puts response
-      response
+      puts "Sending POST request to URL: #{url}"
+      response = HTTParty.post(url, headers: headers, body: body.to_json, debug_output: $stdout)
+      format_response(response)
     end
 
     def self.get(url, headers = nil)
       headers = base_headers if headers.nil?
-      puts url.to_s
+      puts "Sending GET request to URL: #{url}"
       response = HTTParty.get(url, headers)
+      format_response(response)
+    end
+
+    def self.parsed_body(body)
+      body[:VersaoXML] = 1
+      body
+    end
+
+    def self.format_response(original_response)
+      original_response.body.force_encoding('UTF-8')
+      response = original_response.parsed_response
+      response[:status_code] = original_response.code if response.is_a?(Hash)
       puts response
       response
     end
