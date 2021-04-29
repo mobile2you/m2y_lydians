8e69566f3ed4d7195be4615a3b682b3bbce6939a
tsaiyuen <tsaiyuen@gmail.com>
2021-04-29 19:01:08 -0300

    create billet list endpoint

--- lib/m2y_lydians/modules/account.rb
+++ lib/m2y_lydians/modules/account.rb
@@ -34,5 +34,10 @@ module M2yLydians
     def self.generate_billet(body)
       post(base_url + BASE_ACCOUNT_PATH + GENERATE_BILLET_PATH, parsed_body(body))
     end
+
+    # /ContaCartBoletos_37
+    def self.list_billet(body)
+      post(base_url + BASE_ACCOUNT_PATH + LIST_BILLET_PATH, parsed_body(body))
+    end
   end
 end
