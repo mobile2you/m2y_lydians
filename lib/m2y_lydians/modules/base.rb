9b25a318f22508c1ee15fcd2035d8bb45f37d981
Danilo Sobral <danilo.sobral@mobile2you.com.br>
2021-04-30 17:40:30 -0300

    Added timeout handling

--- lib/m2y_lydians/modules/base.rb
+++ lib/m2y_lydians/modules/base.rb
@@ -14,14 +14,22 @@ module M2yLydians
     def self.post(url, body, headers = nil)
       headers = base_headers if headers.nil?
       puts "Sending POST request to URL: #{url}"
-      response = HTTParty.post(url, headers: headers, body: body.to_json, debug_output: $stdout)
+      begin
+        response = HTTParty.post(url, headers: headers, body: body.to_json, debug_output: $stdout)
+      rescue Timeout::Error
+        return timeout_response
+      end
       format_response(response)
     end
 
     def self.get(url, headers = nil)
       headers = base_headers if headers.nil?
       puts "Sending GET request to URL: #{url}"
-      response = HTTParty.get(url, headers)
+      begin
+        response = HTTParty.get(url, headers)
+      rescue Timeout::Error
+        return timeout_response
+      end
       format_response(response)
     end
 
@@ -33,17 +41,28 @@ module M2yLydians
     def self.format_response(original_response)
       original_response.body.force_encoding('UTF-8')
       response = original_response.parsed_response
-      if response.is_a?(Hash)
-        response[:status_code] = original_response.code
-      else
+
+      status_code = original_response.code
+      if status_code.blank?
         # Erro 503 retorna um html em parsed_response
         status_code = response.include?('Error 503') ? 503 : 500
-        response = {}
-        response[:status_code] = status_code
-        response['Descricao'] = 'Houve um erro inesperado, tente novamente mais tarde'
+        description = 'Houve um erro inesperado, tente novamente mais tarde'
       end
+
+      response = { body: response } if response.is_a?(Array)
+      response = {} unless response.is_a?(Hash)
+      response[:status_code] = status_code
+      response['Descricao'] = description if description.present?
+
       puts response
       response
+    end
+
+    def self.timeout_response
+      {
+        status_code: 408,
+        'Descricao': 'Houve um erro inesperado, tente novamente mais tarde'
+      }
     end
   end
 end
